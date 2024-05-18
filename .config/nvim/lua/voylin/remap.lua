0acd0de7babe93b74a82e8890e854168034a1bbc
Voylin <0voylin0@gmail.com>
2024-05-18 10:05:57 +0900

    Create remap.lua

---
+++ .config/nvim/lua/voylin/remap.lua
@@ -0,0 +1,47 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- Insert escaping
+vim.keymap.set('i', 'jj', '<Esc>')
+
+-- Highlight when yanking (copying) text
+--  `yap` to copy entire paragraph
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- Moving visual selection
+vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
+vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
+
+-- Jumping page blocks whilst keeping cursor in middle
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- Searching keeps cursor in middle
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Yanking to clipboard
+vim.keymap.set('n', '<leader>y', '\"+y')
+vim.keymap.set('v', '<leader>y', '\"+y')
+vim.keymap.set('n', '<leader>yap', '\"+yap')
+vim.keymap.set('v', '<leader>yap', '\"+yap')
+
+vim.keymap.set('n', '<leader>f', function()
+	vim.lsp.buf.format()
+end)
+
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set('n', '<leader>S', [[:%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set('i', '<C-c>', '<Esc>')
+
+vim.keymap.set('n', '<leader>g', ':LazyGit<CR>')
+
