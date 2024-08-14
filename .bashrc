9ab0eed7b90b433aa8246f6dadf6a1f6044588be
voylin <0voylin0@gmail.com>
2024-08-14 14:53:50 +0900

    2024-08-14

--- .bashrc
+++ .bashrc
@@ -9,6 +9,27 @@
 #eval "$(ssh-agent -s)"
 #ssh-add ~/.ssh/conoha-voylins-servers.key
 
+git_branch() {
+	git rev-parse --abbrev-ref HEAD 2> /dev/null
+}
+
+git_repo() {
+	git config --get remote.origin.url | rev | cut -d/ -f1 | rev | cut -d. -f1
+}
+
+PS1() {
+  local git_branch=$(git_branch)
+  if [ -n "$git_branch" ]; then
+	  local git_repo=$(git_repo)
+	  PS1='(\[\033[01;32m\]'"$git_repo"'-'"$git_branch"'\[\033[00m\])[\u@\h \W]\$ '
+  else
+    PS1='[\u@\h \W]\$ '
+  fi
+}
+
+PROMPT_COMMAND='PS1'
+
+#PS1='[\u@\h \W]\$ '
 
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
@@ -18,7 +39,6 @@ alias vvim='nvim .'
 
 alias vimgodot='vvim --listen 127.0.0.1:6004'
 alias gozen_vim='cd /storage/programming/GoZen && vim_godot' 
-PS1='[\u@\h \W]\$ '
 
 alias tmuxopen='tmux a -t '
 alias tmuxnew_session='tmux new-session -s '
--- .config/nvim/lua/voylin/lazy/dap.lua
+++ .config/nvim/lua/voylin/lazy/dap.lua
@@ -10,10 +10,10 @@ return {
 		-- Debugging keymaps
 		vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle breakpoint' })
 		vim.keymap.set('n', '<leader><F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-		vim.keymap.set('n', '<leader><F7>', dap.step_over, { desc = 'Debug: Step over' })
-		vim.keymap.set('n', '<leader><F8>', dap.step_into, { desc = 'Debug: Step into' })
-		vim.keymap.set('n', '<leader><F9>', dap.step_out, { desc = 'Debug: Step out' })
-		vim.keymap.set('n', '<leader><F6>', function () dap.terminate() end, { desc = 'Debug: Terminate' })
+		vim.keymap.set('n', '<F7>', dap.step_over, { desc = 'Debug: Step over' })
+		vim.keymap.set('n', '<F8>', dap.step_into, { desc = 'Debug: Step into' })
+		vim.keymap.set('n', '<F9>', dap.step_out, { desc = 'Debug: Step out' })
+		vim.keymap.set('n', '<F6>', function () dap.terminate() end, { desc = 'Debug: Terminate' })
 
 		-- Godot stuff
 		dap.adapters.godot = {
