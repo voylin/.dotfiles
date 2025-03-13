5e85fc295d565d26457576c58cf0f47c812a50f0
voylin <0voylin0@gmail.com>
2025-03-14 04:02:51 +0900

    Markdown update

--- .config/nvim/lua/voylin/markdown.lua
+++ .config/nvim/lua/voylin/markdown.lua
@@ -6,5 +6,14 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.wrap = true
         vim.opt_local.linebreak = true
 		vim.opt_local.colorcolumn = ""  -- This disables the column line
+		vim.opt_local.spell = true
+        vim.opt_local.spelllang = "en_us"
     end,
 })
+
+-- Spelling controls
+-- ]s - move to next misspelled word
+-- [s - move to previous misspelled word
+-- z= - suggest corrections for the word under cursor
+-- zg - add word to spell list (good word)
+-- zw - mark word as wrong
